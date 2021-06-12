82f4caf99bcd36b197ccc1700925de4110b5745c
biswajit-debnath <biswajitdebnath405@gmail.com>
2021-06-12 15:45:08 +0530

    login api done

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,17 +4,25 @@ const {authService} = require("./../services")
 
 const login =async (req, res) => {
     try{
-        const {username} = req.body
+        const {username,password} = req.body
         const userData =await authService.getUserByUserName(username);
         
-        res.json(userData);
+        const validPass = await bcrypt.compare(password, userData.password)
+        if(!validPass)
+            throw {status:401, message: "Unauthorized"}
+        
+        const authToken = jwt.sign(
+            {userId:userData.userId},
+            'this_is_a_secret_token_that_needs_to_be_replaced_later'
+        )
+
+        const resData = {authToken, userName}
+        res.status(200).json(resData);
 
     }catch(err){
         console.log(err);
         res.status(err.status).send(err.message)
     }
-    
-
 }
 
 
--- services/authService.js
+++ services/authService.js
@@ -1,10 +1,9 @@
 const db = require("./../config/config")
 
 const getUserByUserName =async (username)=> {
-    let userData={}
     const res =await db.query(
         `SELECT * FROM users WHERE userName = ($1)`,
-        ['admin2'])
+        [username])
     if(res.rows.length == 0)
         throw {status:404, message: "No user found"}
     return res.rows[0];
